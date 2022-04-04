7bda18a78d8719a1b3cfdeef0ac7d49adecc8901
Gustavo Breve Alessio Nachbar - GNC <gustavo.nachbar@iteris.com.br>
2022-04-03 21:13:44 -0300

    adicionando divs 1 e 2 com style

--- profissionais-saude/src/App.js
+++ profissionais-saude/src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const obterStyle = () => {
+    return {
+      width: 800,
+      margin: "auto",
+      border: "1px solid",
+      borderRadius: 6,
+      padding: "8px 0 8px 0",
+      display: "flex",
+      backgroundColor: "#eee",
+      alignContent: "center",
+      justifyContent: "center",
+      alignItems: "center",
+      marginTop: 5,
+    };
+  };
+
+  const nomes = {
+    profissional1: "Maria da Silva",
+    profissional2: "José da Silva",
+    profissional3: "Jaqueline  Mendes",
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container" style={obterStyle()}>
+        <div>
+          <h3>Profissionais da Saúde</h3>
+          <div
+            style={{
+              padding: "8px 0 8px 0",
+              border: "1px solid #e0e0e0",
+              width: "90%",
+              borderRadius: 6,
+              display: "flex",
+              justifyContent: "space-around",
+              alignItems: "center",
+            }}
+          >
+            <div className="profisisonal">
+              <img src="src/images/medica.jpg" alt=""></img>
+              <p>{nomes[0]}</p>
+            </div>
+            <div className="profisisonal"></div>
+            <div className="profisisonal"></div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }

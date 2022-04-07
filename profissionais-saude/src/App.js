249a64494808ce944b0cd9ef8894d38f48295ebf
Gustavo Breve Alessio Nachbar - GNC <gustavo.nachbar@iteris.com.br>
2022-04-07 17:26:30 -0300

    Todas as imagens aparecendo

--- profissionais-saude/src/App.js
+++ profissionais-saude/src/App.js
@@ -1,9 +1,12 @@
 import "./App.css";
+import medica from "./images/medica.jpg";
 
 function App() {
-  const obterStyle = () => {
+  const style = () => {
     return {
-      width: 800,
+      width: 1200,
+      height: 250,
+      paddingBottom: 20,
       margin: "auto",
       border: "1px solid",
       borderRadius: 6,
@@ -24,14 +27,15 @@ function App() {
   };
   return (
     <div className="App">
-      <div className="container" style={obterStyle()}>
+      <div className="container" style={style()}>
         <div>
-          <h3>Profissionais da Saúde</h3>
+          <h3 style={{ textAlign: "center" }}>Profissionais da Saúde</h3>
           <div
             style={{
               padding: "8px 0 8px 0",
               border: "1px solid #e0e0e0",
-              width: "90%",
+              width: 1100,
+              height: 180,
               borderRadius: 6,
               display: "flex",
               justifyContent: "space-around",
@@ -39,11 +43,17 @@ function App() {
             }}
           >
             <div className="profisisonal">
-              <img src="src/images/medica.jpg" alt=""></img>
-              <p>{nomes[0]}</p>
+              <img src={medica} alt=""></img>
+              <p>{nomes.profissional1}</p>
             </div>
-            <div className="profisisonal"></div>
-            <div className="profisisonal"></div>
+            <div className="profisisonal">
+              <img src="https://cdn-ckope.nitrocdn.com/xzPFVVaGpUTSGVhMONbWgvqbhvcJblQY/assets/static/optimized/rev-cf5893f/wp-content/uploads/2016/12/medico_seu_amigo_hospital-dia-dicas-aproveitar-medico-930x620.jpg"></img>
+              <p>{nomes.profissional2}</p>
+            </div>
+            <div className="profisisonal">
+              <img src={process.env.PUBLIC_URL + "/medica.jpg"}></img>
+              <p>{nomes.profissional3}</p>
+            </div>
           </div>
         </div>
       </div>
